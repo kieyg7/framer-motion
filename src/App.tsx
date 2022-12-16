a16844c6c921be1883565b7754cf68dc7c94fc5c
Kelvin <kieyg7@gmail.com>
2022-12-17 03:46:10 +0900

    layout, layoutId... WOW...

--- src/App.tsx
+++ src/App.tsx
@@ -4,61 +4,15 @@ import styled, {createGlobalStyle} from 'styled-components'
 import {motion, AnimatePresence} from "framer-motion";
 
 function App() {
-
-  const [visible, setVisible] = useState(1);
-
-  const [direction, setDirection] = useState(true)
-
-  const next = () => {
-    setDirection(true);
-    setVisible((prev) => prev === 10 ? 10 : prev + 1)
-  }
-  const prev = () => {
-    setDirection(false);
-    setTimeout(() => setVisible((prev) => prev === 1 ? 1 : prev - 1), 10)
-
-  }
-
-  const boxVariants = {
-    entry: (direction: boolean) => ({
-      x: direction ? 500 : -500,
-      opacity: 0,
-      scale: 0,
-      transition: {duration: 0.5, delay: 0.1}
-    }),
-    center: {
-      x: 0,
-      opacity: 1,
-      scale: 1,
-      // transition: {duration: 1}
-    },
-    exit: (direction: boolean) => ({
-      x: direction ? -500 : 500,
-      opacity: 0,
-      scale: 0,
-      transition: {duration: 0.5, delay: 0.1}
-    })
-  }
-
+  const [clicked, setClicked] = useState(false);
+  const toggleClicked = () => setClicked(!clicked)
   return (
     <div className="App">
       <GlobalStyle />
-      <Wrapper>
-        <AnimatePresence custom={direction} mode="popLayout">
-          <Box
-              custom={direction}
-              variants={boxVariants}
-              initial='entry'
-              animate="center"
-              exit='exit'
-              transition={{type:"spring", bounce: 0.3}}
-              key={visible}
-            >
-            {visible}
-          </Box>
-          </AnimatePresence>
-        <button onClick={prev}>Prev</button>
-        <button onClick={next}>Next</button>
+      <Wrapper onClick={toggleClicked}>
+        <Box>{clicked ? <Circle layoutId="circle" style={{borderRadius: '50%'}}/> : null}</Box>
+        <Box>{clicked ?  null : <Circle layoutId="circle" style={{borderRadius: 0}}/>}</Box>
+
       </Wrapper>
     </div>
   );
@@ -83,7 +37,7 @@ const Wrapper = styled(motion.div)`
   height: 100vh;
     background: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238));
   display: flex;
-  flex-flow: column;
+  flex-flow: row;
   justify-content: center;
   align-items: center;
   button {
@@ -95,19 +49,23 @@ const Wrapper = styled(motion.div)`
 `
 
 const Box = styled(motion.div)`
-   width: 200px;
-   height: 200px;
+   width: 300px;
+   height: 300px;
+  margin: 60px;
    background-color: white;
    border-radius: 15px;
    box-shadow: 0 2px 3px rgba(0,0,0,0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-  position: absolute;
-  top: 100px;
   display: flex;
   flex-flow: column;
   justify-content: center;
   align-items: center;
   font-size: 30px;
  `
-
+const Circle = styled(motion.div)`
+  background-color: #00a5ff;
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+`
 
 export default App;
