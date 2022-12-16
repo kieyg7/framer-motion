7b9da47234aa4df63d8fb8d2f0a15ca2a2e43bad
Kelvin <kieyg7@gmail.com>
2022-12-16 23:50:49 +0900

    staggerChildren

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,31 @@ import './styles/reset.css';
 import styled, {createGlobalStyle} from 'styled-components'
 import {motion} from "framer-motion";
 
-const myVars = {
-  start: {scale: 0},
-  end: {scale: 1, rotateZ: 360, transition: {type:'spring', duration: 2, delay: 1}}
+const boxVariants = {
+  start: {opacity: 0, scale: 0},
+  end: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      type:'spring',
+      duration: 0.5,
+      bounce: 0.5,
+      staggerChildren: 0.2,
+    }}
+}
+
+const circleVariants = {
+  start: {
+    opacity: 0,
+    y: 10,
+  },
+  end: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1
+    }
+  }
 }
 
 function App() {
@@ -13,7 +35,12 @@ function App() {
     <div className="App">
       <GlobalStyle />
       <Wrapper>
-        <Box variants={myVars} initial="start" animate="end"/>
+        <Box variants={boxVariants} initial="start" animate="end">
+          <Circle variants={circleVariants}/>
+          <Circle variants={circleVariants}/>
+          <Circle variants={circleVariants}/>
+          <Circle variants={circleVariants}/>
+        </Box>
       </Wrapper>
     </div>
   );
@@ -46,9 +73,19 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
-  border-radius: 15px;
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0,0,0,0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+`
+
+const Circle = styled(motion.div)`
+  width: 70px;
+  height: 70px;
+  border-radius:  35px;
+  background-color: white;
+  place-self: center;
 `
 
 export default App;
